b153c96cb6f0439b46ee6720cb01b99529917826
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-05-08 23:51:20 +0300

    B-Tree: add method for search and split

---
+++ Trees/BTree/BTree.cs
@@ -0,0 +1,56 @@
+﻿namespace Algorithms_CS.Trees.BTree
+{
+    public class BTree
+    {
+        BNode root = null;
+        public const int T = 2;
+
+        public (BNode resNode, int positionKey) ?Search(BNode node, int key)
+        {
+            int i = 0;
+            while(i <= node.Count && key > node.Keys[i])                         
+                i++;
+                           
+            if (i <= node.Count && key == node.Keys[i])
+                return (node, i);
+
+            if (node.IsLeaf)
+                return null;
+            else
+                return Search(node.Kids[i], key);
+        }
+
+        public void SplitChild(BNode x, int i, BNode y)
+        {
+            BNode z = new BNode(y.IsLeaf);
+
+            z.Count = T - 1;
+            for (int j = 0; j < z.Count; j++)
+            {
+                z.Keys[j] = y.Keys[T + j];
+            }
+
+            if (!z.IsLeaf)
+            {
+                for(int j = 0; j < T; j++)
+                {
+                    z.Kids[j] = y.Kids[j + T];
+                }
+            }
+            y.Count = T - 1;
+
+            for(int j = x.Count; j >= i+1; j--)
+            {
+                x.Kids[j+1] = x.Kids[j];
+            }
+            x.Kids[i + 1] = z;
+            
+            for(int j = x.Count-1; j >= i; j--)
+            {
+                x.Keys[j + 1] = x.Keys[j];
+            }
+            x.Keys[i] = y.Keys[T];
+            x.Count++;
+        }
+    }
+}
---
+++ Trees/BTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Algorithms_CS.Trees.BTree
+{
+    public class BNode
+    {
+        public const int T = 2;
+
+        public int Count; 
+        public int[] Keys;
+        public BNode[] Kids;
+        public bool IsLeaf;
+
+        public BNode(bool isLeaf)
+        {
+            this.IsLeaf = isLeaf;
+            this.Keys = new int[2 * T];
+            this.Kids = new BNode[2 * T + 1];
+            this.Count = 0;
+        }
+    }
+}
